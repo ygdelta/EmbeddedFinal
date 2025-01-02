ee72393450cd9e56f7e463d541e948b5aebd0a90
ygdela <yt91205@gmail.com>
2025-01-02 20:35:59 +0800

    done html

--- app.py
+++ app.py
@@ -1,23 +1,50 @@
 from flask import Flask, jsonify, render_template
 from max30100 import MAX30100
 from ds18b20 import DS18B20
+from mpu6050 import mpu6050
 
 app = Flask(__name__)
 
 try:
-    mx30 = MAX30100()
-    mx30.begin()
+  mx30 = MAX30100()
+  mx30.begin()
+  ds18 = DS18B20()
+  mpu6050 = mpu6050(0x68)
 except Exception as e:
-    print("Init Error: %")
+  print("Init Error: {}".format(e))
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+  return render_template('index.html')
 
 @app.route('/max30100', methods=['GET'])
 def getMax30100():
-    res = jsonify({
-        'heart_rate': mx30.heart_rate(),
-        'spo2': mx30.spo2()
-        })
-    return res
+  res = jsonify({
+    'heart_rate': mx30.heart_rate(),
+    'spo2': mx30.spo2()
+  })
+  return res
+
+@app.route('/ds18b20', methods=['GET'])
+def getDS18B20():
+  res = jsonify({
+    'temperature': ds18.get_temperature()
+  })
+  return res
+
+@app.route('/mpu6050', methods=['GET'])
+def getMPU6050():
+  accel_data = mpu6050.get_accel_data()
+  status = 'normal'
+  x = accel_data['x']
+  y = accel_data['y']
+  z = accel_data['z']
+  if abs(x) > 10 and abs(y) > 10 and abs(z) > 10:
+    status = 'danger'
+  res = jsonify({
+    'x': x,
+    'y': y,
+    'z': z,
+    'status': status 
+  })    
+  return res
--- static/index.js
+++ static/index.js
@@ -8,5 +8,30 @@ $(document).ready(function() {
         $('#spo2').text('SpO2: ' + response.spo2);
       }
     })
-  }, 1000);
+  }, 2000);
+
+  setInterval(function() {
+    $.ajax({
+      url: 'http://127.0.0.1:5000/ds18b20',
+      type: 'GET',
+      success: function(res) {
+        $('#temp').text('Temperature: ' + res.temperature);
+      }
+    });
+  }, 2000);
+
+  setInterval(function() {
+    $.ajax({
+      url: 'http://127.0.0.1:5000/mpu6050',
+      type: 'GET',
+      success: function(res) {
+        let status = res.status;
+        $('#state').text('State: ' + status);
+        if ( status == 'danger' ) {
+          window.alert('Dangerous situation detected!');
+        }
+        $('#accel').text('x:' + res.x + ', y: ' + res.y + ', z: ' + res.z);
+      }
+    });
+  }, 2000);
 });
