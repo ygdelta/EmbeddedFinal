cc3b6a86d287e9bed8b0fcd6ed4c7e9e34ef8360
ygdela <yt91205@gmail.com>
2025-01-02 20:11:21 +0800

    add ds18b20 lib

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 from max30100 import MAX30100
+from ds18b20 import DS18B20
 
 app = Flask(__name__)
 
@@ -8,6 +9,10 @@ try:
     mx30.begin()
 except Exception as e:
     print("Init Error: %")
+
+@app.route('/')
+def home():
+    return render_template('index.html')
 
 @app.route('/max30100', methods=['GET'])
 def getMax30100():
