897be7f5a48887e325da53b2d2cb5756f0732322
ygdelta <yt91205@gmail.com>
2025-01-03 22:24:19 +0800

    modify js

--- static/index.js
+++ static/index.js
@@ -4,8 +4,10 @@ $(document).ready(() => {
       url: 'http://127.0.0.1:5000/max30100',
       type: 'GET',
       success: (response) => {
-        CheckBPM(response.heart_rate);
-        CheckO2(response.spo2);
+        heart = response.heart_rate;
+        spo2 = response.spo2;
+        CheckBPM(heart);
+        CheckO2(spo2);
       },
       error: (err) => {
         $('#heart').text('Waiting');
@@ -32,6 +34,7 @@ $(document).ready(() => {
       url: 'http://127.0.0.1:5000/mpu6050',
       type: 'GET',
       success: function(res) {
+        let res = res;
         CheckState(res.status);
         $('#accel').text('x:' + res.x + ', y: ' + res.y + ', z: ' + res.z);
       },
@@ -40,50 +43,55 @@ $(document).ready(() => {
       }
     });
   }, 3000);
+
+  let CheckBPM = (bpm) => {
+    if ( !typeof bpm === 'number' ) {
+      $('#heart').text('Waiting');
+      return;
+    }
+    if ( bpm > 150 || bpm < 40 ) {
+      $('#heart').css('color', 'red');
+    }
+    else {
+      $('#heart').css('color', 'white');
+    }
+  }
+
+  let CheckO2 = (spo2) => {
+    if ( !typeof spo2 === 'number' ) {
+      $('#spo2').text('Waiting');
+      return;
+    }
+    if ( spo2 < 95 ) {
+      $('#spo2').css('color', 'red');
+    }
+    else {
+      $('#spo2').css('color', 'white');
+    }
+  }
+
+  let CheckTemp = (temp) => {
+    if ( !typeof temp === 'number' ) {
+      $('#temp').text('None');
+      return;
+    }
+    if ( temp < 35 || temp > 37.5 ) {
+      $('#temp').css('color', 'red');
+    }
+    else {
+      $('#temp').css('color', 'white');
+    }
+  }
+
+  let CheckState = (state) => {
+    $('#state').text(state);
+    if ( state == 'danger' ) {
+      window.alert('Dangerous situation detected!');
+      $('#state').css('color', 'red');
+    }
+    else {
+      $('#state').css('color', 'white');
+    }
+  }
 });
 
-let CheckBPM = (bpm) => {
-  if ( bpm > 150 || bpm < 40 ) {
-    $('#heart').css('color', 'red');
-  }
-  else {
-    $('#heart').css('color', 'white');
-  }
-}
-
-let CheckO2 = (spo2) => {
-  if ( !typeof spo2 === 'number' ) {
-    $('#spo2').text('None');
-    return;
-  }
-  if ( spo2 < 95 ) {
-    $('#spo2').css('color', 'red');
-  }
-  else {
-    $('#spo2').css('color', 'white');
-  }
-}
-
-let CheckTemp = (temp) => {
-  if ( !typeof temp === 'number' ) {
-    $('#temp').text('None');
-    return;
-  }
-  if ( temp < 35 || temp > 37.5 ) {
-    $('#temp').css('color', 'red');
-  }
-  else {
-    $('#temp').css('color', 'white');
-  }
-}
-
-let CheckState = (state) => {
-  $('#state').text(state);
-  if ( state == 'danger' ) {
-    window.alert('Dangerous situation detected!');
-    $('#state').css('color', 'red');
-  }
-  else {
-    $('#state').css('color', 'white');
-  }
-}
